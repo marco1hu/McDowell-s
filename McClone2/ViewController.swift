e09191232cd8e9a123f09e5049d232124eb4caaf
Marco <humarco1310@gmail.com>
2024-04-21 13:40:29 +0200

    Initial Commit

---
+++ McClone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  McClone2
+//
+//  Created by IFTS40 on 21/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
